f4b73d1ee135e527f621c7ddfd4a9dff91e13614
Mariana <marianafmarques@ufmg.br>
2020-02-23 19:56:24 -0300

    Final Version (maybe)

--- src/1.Wifi.ino
+++ src/1.Wifi.ino
@@ -1,8 +1,8 @@
 #include <ESP8266WiFi.h>
 
 /*Put your SSID & Password*/
-const char* ssid = "Casa_Nana";  // Enter SSID here
-const char* password = "74747474";  //Enter Password here
+const char* ssid = "OnePlus 6";  // Enter SSID here
+const char* password = "123467890";  //Enter Password here
 
 // const char* ssid = "OnePlus 6";  // Enter SSID here
 // const char* password = "123467890";  //Enter Password here
